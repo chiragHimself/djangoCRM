c2c5d756b805791bc55fef6672d870841304a361
chiragHimself <itschirag21@gmail.com>
2025-03-17 22:47:57 +0530

    update and add record utility added ,# final commit

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login , logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm,AddRecordForm
 from .models import Record
+
 
 # Create your views here.
 def home(request):
@@ -60,3 +61,29 @@ def delete_record(request,pk):
     else:
         messages.success(request , "You need to log in first.")
         return redirect('home')
+    
+def add_record(request):
+    if(request.method == "POST"):
+        form = AddRecordForm(request.POST)
+        if form.is_valid():
+           form.save()
+           messages.success(request,"Record added successfully.")
+           return redirect('home')
+    else:
+        form = AddRecordForm()
+        return render(request, 'add_record.html', {'form':form})
+
+    return render(request, 'register.html', {'form':form})
+
+def update_record(request,pk):
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Record Has Been Updated!")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('home')
